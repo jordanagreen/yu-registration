63fd0b283c57525a953e3418d37059cd1da664cb
Jordan Green <thegreenjordan@gmail.com>
2016-05-26 21:35:32 +0000

    Add searching offerings by department, instructor, number, and semester

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,14 @@
 class Course < ActiveRecord::Base
     has_many :offerings, inverse_of: :course
+    
+     def self.search_offerings(search)
+        # search by department name, instructor, course number, and semester
+        department = search[:department] || ''
+        instructor = search[:instructor] || ''
+        course_number = search[:course_number] || ''
+        semester = search[:semester] || ''
+        joins(:offerings).where("department LIKE (?) AND course_number LIKE (?)
+            AND offerings.professor_last_name LIKE (?) AND offerings.semester = ?", 
+            "%#{department}%", "%#{course_number}%", "%#{instructor}%", semester)
+     end
 end

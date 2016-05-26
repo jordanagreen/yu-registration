c36ba9a9381fbcfc799429c826aeb479f19ff2ea
Jordan Green <thegreenjordan@gmail.com>
2016-05-26 21:48:12 +0000

    Reduce spots available by 1 when enrolling

--- app/models/offering.rb
+++ app/models/offering.rb
@@ -2,4 +2,10 @@ class Offering < ActiveRecord::Base
     enum semester: [:fall, :spring, :summer]
     belongs_to :course, inverse_of: :offerings
     validates :crn, uniqueness: true
+    
+    def enroll
+        if (self.spots_available > 0) then 
+            self.decrement! :spots_available, 1
+        end
+    end
 end

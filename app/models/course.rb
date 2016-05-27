87997cc37ab97f850ac6ddb47b416480156eb3e3
Jordan Green <thegreenjordan@gmail.com>
2016-05-27 07:08:14 +0000

    Fix no given semester not being a wildcard in searches

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,9 +6,9 @@ class Course < ActiveRecord::Base
         department = search[:department] || ''
         instructor = search[:instructor] || ''
         course_number = search[:course_number] || ''
-        semester = search[:semester] || ''
+        semester = search[:semester] || '_'
         joins(:offerings).where("department LIKE (?) AND course_number LIKE (?)
-            AND offerings.professor_last_name LIKE (?) AND offerings.semester = ?", 
+            AND offerings.professor_last_name LIKE (?) AND offerings.semester LIKE ?", 
             "%#{department}%", "%#{course_number}%", "%#{instructor}%", semester)
      end
 end

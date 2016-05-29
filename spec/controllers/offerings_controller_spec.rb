62a923371b3b0e0c8f98805de74cbaad047b625a
Jordan Green <thegreenjordan@gmail.com>
2016-05-29 16:16:01 +0000

    Add tests for offerings controller

--- spec/controllers/offerings_controller_spec.rb
+++ spec/controllers/offerings_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe OfferingsController, type: :controller do
+    
+    describe 'GET index' do
+        it 'sets @offerings' do
+            get :index
+            expect(assigns :offerings).to_not be_nil
+        end
+            
+        it 'renders the index' do
+            get :index
+            expect(response).to render_template("index")
+        end
+    end
+    
+    describe 'GET enroll' do
+        before :each do 
+            @c = Course.create(department: "COM", course_number: "1000", title: "Intro") 
+            @o = Offering.create(course: @c, semester: 1, location: "Belfer 400",
+                section: 321, professor_last_name: "Kelly", spots_available: 10, crn: 5000)
+        end
+        
+        it 'redirects to offerings index' do
+            get :enroll, :id => @o.id
+            expect(response).to redirect_to offerings_path
+        end
+        
+        it 'decrements the spots available' do
+            get :enroll, :id => @o.id
+            @o.reload
+            expect(@o.spots_available).to eql 9
+        end
+    end
 
 end
